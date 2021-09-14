fdd2f94bd7803a475527b8487117d570df9f3530
DiogoAlvesOliveira <diogo.dream89@gmail.com>
2021-09-13 23:59:12 -0300

    token added

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController';
+import loginRequeired from '../middlewares/loginRequeired';
 
 const router = new Router();
 router.post('/', userController.store);
-router.get('/', userController.index);
+router.get('/', loginRequeired, userController.index);
 router.get('/:id', userController.show);
 router.put('/:id', userController.update);
 router.delete('/:id', userController.delete);
